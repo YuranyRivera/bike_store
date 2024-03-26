c7e5b5209a132eaa53a884cd959db694c9a35e6f
YuranyRivera <160735735+YuranyRivera@users.noreply.github.com>
2024-03-26 07:43:26 -0500

    Navbar hecho

---
+++ Frontend/src/assets/components/Navbar/Navbar.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import './Navbar.css';
+import logoBike from '../../img/logobikestore.png';
+import carrito from '../../img/carrito.png';
+import usuario from '../../img/iconusuario.png';
+
+function Navbar() {
+  const [menuVisible, setMenuVisible] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuVisible(!menuVisible);
+    document.body.style.overflow = menuVisible ? 'auto' : 'hidden';
+  };
+
+  return (
+    <main>
+      <section className="contenido">
+        <header className="header">
+          <img src={logoBike} alt="Logo" className="logo" />
+          <div className="container">
+            <div className="btn">
+              <input type="text" placeholder="Buscar" />
+              <i className="fa fa-search"></i>
+            </div>
+          </div>
+          <a href="/HTML/carrito.html">
+            <img src={carrito} alt="carrito" className="logo1" />
+          </a>
+          <a href="">
+            <img src={usuario} alt="usuario" className="logo1" />
+          </a>
+        </header>
+      </section>
+
+      <header className="header2">
+        <button id="abrir" className="abrir-menu" onClick={toggleMenu}>
+          <i className="bi bi-list"></i>
+        </button>
+        <nav className={`nav ${menuVisible ? 'visible' : ''}`} id="nav">
+          <button className="cerrar-menu" id="cerrar" onClick={toggleMenu}>
+            <i className="bi bi-x"></i>
+          </button>
+          <ul className="nav-list">
+            <li><a href="#">HOME</a></li>
+            <li><a href="/HTML/bicicletas.html">BICICLETAS</a></li>
+            <li><a href="/HTML/accesorio.html">ACCESORIOS</a></li>
+            <li><a href="/HTML/vestuario.html">VESTUARIO</a></li>
+            <li><a href="#">DESCUENTO</a></li>
+            <li><a href="/HTML/contactanos.html">CONTACTANOS</a></li>
+          </ul>
+        </nav>
+      </header>
+    </main>
+  );
+}
+
+export default Navbar;
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -6,16 +6,13 @@ import Catalogo from './assets/components/Catalogo/Catalogo.jsx'
 import Filtro from './assets/components/Filtro/Filtro.jsx'
 import FondoBici from './assets/components/FondoBici/FondoBici.jsx'
 import Footer from './assets/components/Footer/Footer.jsx'
+import Navbar from './assets/components/Navbar/Navbar.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-<<<<<<< HEAD
+
     <Navbar/>
-=======
 
-    <Footer/>
-
->>>>>>> 39941b7041d820d0caf6aa3a0b5a7d72661b623c
   </React.StrictMode>,
 )
